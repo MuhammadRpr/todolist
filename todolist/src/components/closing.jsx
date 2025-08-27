e7f41dee89db0066d3337b3a7efe7502b9e31a9a
MuhammadRpr <muhammadqu017@gmail.com>
2025-08-27 14:22:04 +0700

    updt

--- todolist/src/components/closing.jsx
+++ todolist/src/components/closing.jsx
@@ -1,6 +1,6 @@
 function Closing({ text }) {
     return (
-        <header className="bg-orange-400 text-center py-4 text-xl font-semibold text-white">
+        <header className="bg-orange-400 text-center py-4 text-xl font-semibold text-white rounded-2xl">
             {text}
         </header>
     );
